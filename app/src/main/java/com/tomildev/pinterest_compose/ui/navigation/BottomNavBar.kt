b8875d0116b8805c601c00dbaa706a8e759901ea
Milton-Peluffo <milton.peluffoc@gmail.com>
2025-12-21 22:14:47 -0500

    feat(navigation bar): Implement dynamic icons for BottomNavBar

--- app/src/main/java/com/tomildev/pinterest_compose/ui/navigation/BottomNavBar.kt
+++ app/src/main/java/com/tomildev/pinterest_compose/ui/navigation/BottomNavBar.kt
@@ -1,32 +1,49 @@
 package com.tomildev.pinterest_compose.ui.navigation
 
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.tomildev.pinterest_compose.R
 
 @Composable
 fun BottomNavBar() {
     val itemList = listOf<NavItem>(
-        NavItem(name = "Home", icon = painterResource(R.drawable.ic_home_filled)),
-        NavItem(name = "Search", icon = painterResource(R.drawable.ic_home_filled)),
-        NavItem(name = "Add", icon = painterResource(R.drawable.ic_home_filled)),
-        NavItem(name = "Inbox", icon = painterResource(R.drawable.ic_home_filled)),
-        NavItem(name = "Profile", icon = painterResource(R.drawable.ic_home_filled)),
+        NavItem(name = "Home",
+            iconFilled = R.drawable.ic_home_filled,
+            iconOutlined = R.drawable.ic_home_outlined),
+        NavItem(name = "Search",
+            iconFilled = R.drawable.ic_search_thick,
+            iconOutlined = R.drawable.ic_search),
+        NavItem(name = "Add",
+            iconFilled = R.drawable.ic_plus,
+            iconOutlined = R.drawable.ic_plus),
+        NavItem(name = "Inbox",
+            iconFilled = R.drawable.ic_inbox_filled,
+            iconOutlined = R.drawable.ic_inbox_outlined),
+        NavItem(name = "Profile",
+            iconFilled = R.drawable.ic_home_filled,
+            iconOutlined = R.drawable.ic_home_filled),
     )
 
 var selectedIndex by remember { mutableIntStateOf(0) }
 
     NavigationBar(
-        contentColor = Color.White
+        modifier = Modifier
+            .height(95.dp)
+        ,
+        contentColor = Color.Transparent,
     ) {
         itemList.forEachIndexed { index, item ->
             PinterestCompose(navItem = item, isSelected = index == selectedIndex){
@@ -39,9 +56,19 @@ var selectedIndex by remember { mutableIntStateOf(0) }
 @Composable
 fun RowScope.PinterestCompose(navItem: NavItem,isSelected: Boolean, onItemclick: () -> Unit) {
 
+    val iconId = if (isSelected) navItem.iconFilled else navItem.iconOutlined
+
     NavigationBarItem(
-        selected = false,
-        onClick = {},
-        icon = { Icon(navItem.icon, contentDescription = "") }
+        selected = isSelected,
+        onClick = onItemclick,
+        icon = {
+            Icon(
+                painter = painterResource(id = iconId), contentDescription = navItem.name
+            )
+        }
+    )
+    NavigationBarItemDefaults.colors(
+        selectedIconColor = Color.White,
+        indicatorColor = Color.Transparent
     )
 }
--- app/src/main/java/com/tomildev/pinterest_compose/ui/navigation/NavItem.kt
+++ app/src/main/java/com/tomildev/pinterest_compose/ui/navigation/NavItem.kt
@@ -1,5 +1,8 @@
 package com.tomildev.pinterest_compose.ui.navigation
 
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.annotation.DrawableRes
 
-data class NavItem(val name: String, val icon: Painter)
+data class NavItem(
+    val name: String,
+    @DrawableRes val iconFilled: Int,
+    @DrawableRes val iconOutlined: Int)
