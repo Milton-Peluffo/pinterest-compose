4a31764aa7721958ac680a61db06a23f7165aa5f
Milton-Peluffo <milton.peluffoc@gmail.com>
2025-12-30 18:22:27 -0500

    style: improve topBar styles

--- app/src/main/java/com/tomildev/pinterest_compose/ui/components/TopBar.kt
+++ app/src/main/java/com/tomildev/pinterest_compose/ui/components/TopBar.kt
@@ -1,20 +1,29 @@
 package com.tomildev.pinterest_compose.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -43,31 +52,65 @@ fun TopBar(modifier: Modifier = Modifier) {
             .height(60.dp),
         title = {
             LazyRow(
-                horizontalArrangement = Arrangement.spacedBy(25.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(23.dp),
             ) {
-                items(topBartitles) { title ->
-                    Text(
-                        text = title,
-                        fontSize = 15.sp,
-                        color = Color.White,
-                        fontWeight = FontWeight.SemiBold,
-                        modifier = Modifier.clickable(
-                            interactionSource = remember { MutableInteractionSource() },
-                            indication = null,
-                            onClick = {})
+                itemsIndexed(topBartitles) { index, title ->
+
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        var selectedIndex by remember { mutableIntStateOf(0) }
+                        Text(
+                            text = title,
+                            fontSize = 15.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.SemiBold,
+                            modifier = Modifier.clickable(
+                                interactionSource = remember { MutableInteractionSource() },
+                                indication = null,
+                            )
+                            {
+                                selectedIndex = index
+                            }
+                        )
+                        if (index == selectedIndex) {
+                            Box(Modifier
+                                .height(2.dp)
+                                .size(50.dp, 2.dp)
+                                .background(Color.White)
+                            )
+                        }
+
+                    }
+
+                }
+                item {
+                    VerticalDivider(
+                        modifier = Modifier.height(20.dp),
+                        thickness = 1.5.dp,
+                        color = Gray
                     )
                 }
                 item {
-                    Button(onClick = {}) {
+                    Box(
+                        modifier = Modifier
+                            .size(30.dp)
+                            .clip(shape = RoundedCornerShape(35))
+                            .background(color = Gray)
+                            .clickable(
+                                onClick = {},
+                                interactionSource = remember { MutableInteractionSource() },
+                                indication = null
+                            ),
+                        contentAlignment = Alignment.Center
+                    ) {
                         Icon(
                             painter = painterResource(R.drawable.ic_magic),
-                            modifier = Modifier.size(25.dp),
-                            tint = Gray,
+                            modifier = Modifier.size(16.dp),
                             contentDescription = null
                         )
                     }
                 }
             }
-        },
+        }
     )
 }
